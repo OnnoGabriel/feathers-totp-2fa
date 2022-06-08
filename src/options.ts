38cbaa037736d98a573cc00474017850384e632c
Yannick Marcon <yannick.marcon@obiba.org>
2022-06-08 22:56:39 +0200

    Default options fixed

--- src/options.ts
+++ src/options.ts
@@ -2,6 +2,5 @@ export const defaultOptions = {
   usersService: "/users",
   secretFieldName: "totp2faSecret",
   requiredFieldName: "totp2faRequired",
-  cryptoSetting: "crypto",
   applicationName: "Feathers App"
 };
